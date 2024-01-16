77782a0022bacfce78aa1c8fe38b55117b8315c8
Jander Gomez <jandergb.30@gmail.com>
2024-01-16 09:02:55 +0100

    create db.json

---
+++ src/hooks/useApi.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+const useApi = async (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  try {
+    const fetchData = await fetch(url)
+    const response = await fetchData.json();
+  
+    if (!response) 
+      setData(response);
+    
+  } catch (error) {
+    setError(error);
+  }
+
+
+  return { data, error }
+}
+
+export default useApi
