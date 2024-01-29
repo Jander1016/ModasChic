a3c80a2b19c928600d455bdaae3e077b1d330e7b
Jander Gomez <jandergb.30@gmail.com>
2024-01-29 13:54:51 +0100

    merge develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import Navbar from "./components/Navbar/Navbar";
 import ProductsFront from "./pages/frontProducts";
-// import ProductsDashboard from "./pages/products";
-/* import { Products } from "./components/ProductList";
- */import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import ProductsDashboard  from "./pages/products";
-// import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductsDashboard from "./pages/products";
+import { ProductProvider } from "./context/productContext.jsx";
 
 export default function App() {
   return (
     <>
-    <BrowserRouter>
-       <Navbar/>
-        <Routes>
-          <Route path='/' element={<ProductsFront/>}/>
-          <Route path='/control-productos' element={<ProductsDashboard/>}/>
-        </Routes>
+      <BrowserRouter>
+        <Navbar />
+        <ProductProvider>
+          <Routes>
+            <Route path="/" element={<ProductsFront />} />
+            <Route path="/control-productos" element={<ProductsDashboard />} />
+          </Routes>
+        </ProductProvider>
       </BrowserRouter>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ProductProvider } from './context/productContext.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <ProductProvider>
         <App />
-    </ProductProvider>
 )
